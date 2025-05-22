9ba36a83463521b7df0b34b9d3aadea92e9e4af7
shenxh <xianhui.shen@eslink.com>
2025-05-22 09:07:19 +0800

    feat: energy-h5

--- assets/index-legacy.6c2d5856.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.4d625e31.js","./index-legacy.08e0a554.js"],(function(e){"use strict";var n,t,c,s,r;return{setters:[function(){},function(e){n=e._,t=e.o,c=e.a,s=e.b,r=e.n}],execute:function(){var i={class:"gc-body"},a={class:"gc-body-container"};e("G",n({},[["render",function(e,n){return t(),c("div",i,[s("div",a,[r(e.$slots,"default")])])}]]))}}}));
---
+++ assets/index.8946d204.js
@@ -0,0 +1,1 @@
+import"./index.c0aa7530.js";import{_ as o,o as s,a as c,b as t,n as a}from"./index.343c62d9.js";const r={},n={class:"gc-body"},d={class:"gc-body-container"};function _(e,i){return s(),c("div",n,[t("div",d,[a(e.$slots,"default")])])}var m=o(r,[["render",_]]);export{m as G};
