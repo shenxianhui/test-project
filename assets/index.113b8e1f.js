679e7d9c79e23dd916154cac37fbbddacc7dd8bf
shenxh <xianhui.shen@eslink.com>
2025-05-23 09:11:10 +0800

    sc

---
+++ assets/index.113b8e1f.js
@@ -0,0 +1,1 @@
+import"./index.14dd600b.js";import{_ as o,o as s,a as c,b as t,n as a}from"./index.56fb6adf.js";const r={},n={class:"gc-body"},d={class:"gc-body-container"};function _(e,i){return s(),c("div",n,[t("div",d,[a(e.$slots,"default")])])}var m=o(r,[["render",_]]);export{m as G};
